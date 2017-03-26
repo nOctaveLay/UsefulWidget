ce68a49ef0f3f4698b258fff2281b3381345a126
Clear <wjddkdml1124@naver.com>
2017-03-27 02:07:47 +0900

    revised List.py not finished yet

--- List.py
+++ List.py
@@ -1,21 +1,57 @@
-
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
 # Van-st
+#not finished yet
 import time
-start_time = time.time()
-temp_list = []
-sum_list = 0
-print("computing the time....")
-while True:
-	input_sen = input("if you finish the work, you must input the message 'end': ")
-	# if input_sen == 'a':
-	# 	sum_list += 10
-	if input_sen == 'end':
-		end_time = time.time()
-		delta_time = round(end_time - start_time,0)
-		delta_hour = int(delta_time // 60 // 60)
-		delta_min = int((delta_time // 60 )% 60)
-		delta_sec = int(delta_time % 60)
-		print("Running time ",str(delta_hour)+":"+str(delta_min)+":"+str(delta_sec))
-		print("The score is",sum_list)
-		break
-	print("plus the score ")
+class Ui_Form(object):
+	def setupUi(self,Form):
+		w = 400
+		h = 300
+		Form.setObjectName("Form")
+		Form.resize(w,h)
+		self.label = QLabel(Form)
+		self.label.setAlignment(Qt.AlignCenter)
+		self.label.setGeometry(QRect(0,0,w,h))#x,y,w,h text is printed on left bottom of box
+		self.label.setObjectName("label")
+
+class MainControl(object):
+	def __init__(self,time):
+		self.start_time = time
+		self.count_time = 0
+	def run(self):
+		while True:
+			input_sen = input("if you finish the work, you must input the message 'end': ")
+			# if input_sen == 'a':
+			# 	sum_list += 10
+			if input_sen == 'end':
+				end_time = time.time()
+				delta_time = round(end_time - self.start_time,0)
+				delta_hour = int(delta_time // 60 // 60)
+				delta_min = int((delta_time // 60 )% 60)
+				delta_sec = int(delta_time % 60)
+				self.text1 = "Running time ",str(delta_hour)+":"+str(delta_min)+":"+str(delta_sec)
+				# self.text2 = "The score is"+sum_list
+				break
+			# print("plus the score ")
+
+if __name__ == "__main__":
+	start_time = time.time()
+	app = QApplication(sys.argv)
+	Form = QWidget()
+	ui = Ui_Form()
+	control = MainControl(start_time)
+	ui.setupUi(Form)
+	Form.show()
+	def update_label():
+		count = control.count_time
+		count_hour = int(count // 60 // 60)
+		count_min = int((count // 60) % 60)
+		count_sec = int(count % 60)
+		ui.label.setText("running time is "+str(count_hour)+":"+str(count_min)+":"+str(count_sec))
+		control.count_time += 1
+	timer = QTimer()
+	timer.timeout.connect(update_label)
+	timer.start(1000)
+
+	sys.exit(app.exec_())
